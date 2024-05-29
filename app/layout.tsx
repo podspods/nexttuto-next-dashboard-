2c2cc92b69fd92c1a2b8469153b87d01ddc6a0ff
Emile Destrieux <emile.destrieux@systrangroup.com>
2024-05-29 22:46:34 +0200

    28052024

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-       <body className={`${inter.className} antialiased`}>{children}</body>
+       <body className={`${inter.className} antialiased bg-blue-500`}>{children}</body>
     </html>
   );
 }
